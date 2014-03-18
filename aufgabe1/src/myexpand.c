0496abeb7b5cee263c58ee68895c37008544d1ea
David PFAHLER <y1126287@apps1.tilab.tuwien.ac.at>
2014-03-18 17:35:54 +0100

    [FEATURE] more updates

--- aufgabe1/src/myexpand.c
+++ aufgabe1/src/myexpand.c
@@ -7,11 +7,13 @@
 /* === INCLUDES === */
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h> /* for INT_MIN, INT_MAX */
 
 /* === CONST === */
+char* pgm_name;
 
 /* === PROTOTYPES === */
-char **parseInput(int argc, char **argv, int *tabstop);
+char **parseInput(int argc, char **argv, unsigned int *tabstop);
 
 /**
  * The main entry point of the program.
@@ -22,12 +24,18 @@ char **parseInput(int argc, char **argv, int *tabstop);
  */
 int main(int argc, char **argv)
 {
-    int tabstop = 8;
+    if(argc > 0){
+        pgm_name = argv[0];
+    } else{
+        (void) fprintf(stderr, "Fatal error. There is no program name Cannot continue.\n");
+        exit(EXIT_FAILURE);
+    }
+    unsigned int tabstop = 8;
     char** files = parseInput(argc,argv,&tabstop);
     if(!files){
         exit(EXIT_FAILURE);    
     }
-    printf("Hello, world!\n");
+    printf("Tabstop = %d!\n",tabstop);
     return 0;
 }
 
@@ -39,6 +47,31 @@ int main(int argc, char **argv)
  * @param tabstop The number of spaces to replace one tab
  * @return Pointer to an array of files
  */
-char **parseInput(int argc, char **argv, int *tabstop){
-   return NULL; 
+char **parseInput(int argc, char **argv, unsigned int *tabstop){
+    char* usage = "SYNOPSIS:\n\tmyexpand [-t tabstop] [file...]";
+    int firstFile = 1;
+    //Check if there are too less arguments
+    if (argc < 2){
+        (void) fprintf(stderr, "%s: Too less arguments.\n%s\n", pgm_name, usage);
+        return NULL; 
+    }
+    printf("%s\n",strcmp(argv[1],"-t"));
+    //Check if the -t flag is set
+    if (strcmp(argv[1],"-t") == 0){
+        long buff = strtol(argv[2],NULL,10);
+        if (buff >= INT_MIN && buff <= INT_MAX) {
+            *tabstop = buff;
+            firstFile = 3;
+            (void) printf("%d\n",buff);
+        }
+        else {
+            (void) fprintf(stderr, "%s: The tabstop was too big.\n", pgm_name);
+            return NULL;
+        }
+    }
+    else{
+        (void) printf("%s\n",argv[1]);
+    }
+	return argv+firstFile;
+
 }
