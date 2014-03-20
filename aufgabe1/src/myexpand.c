293120ea66168c03680a41a7e006ccd0aef5802c
David PFAHLER <y1126287@apps1.tilab.tuwien.ac.at>
2014-03-20 15:56:44 +0100

    [FEATURE] file operations untestet

--- aufgabe1/src/myexpand.c
+++ aufgabe1/src/myexpand.c
@@ -19,6 +19,7 @@ static const char usage[] = "SYNOPSIS:\n\tmyexpand [-t tabstop] [file...]";
 
 /* === PROTOTYPES === */
 int parseInput(int argc, char **argv, unsigned int *tabstop, unsigned int *firstFile);
+int replaceTabsOfFile(const char* filename,const int tabstop);
 
 /**
  * The main entry point of the program.
@@ -30,51 +31,25 @@ int parseInput(int argc, char **argv, unsigned int *tabstop, unsigned int *first
 int main(int argc, char **argv)
 {
     unsigned int tabstop = 8;
-    static const char usage[] = "SYNOPSIS:\n\tmyexpand [-t tabstop] [file...]";
-    int firstFile = 1;
+    unsigned int firstFile = 1;
 	
 	//Check if the program exists
     if(argc > 0){
         pgm_name = argv[0];
     } else{
-        (void) fprintf(stderr, "Fatal error. There is no program name Cannot continue.\n");
+        (void) fprintf(stderr, "Fatal error. There is no program name. Cannot continue.\n");
         exit(EXIT_FAILURE);
     }
-    //Check if there are too less arguments
-    if (argc < 2){
-        (void) fprintf(stderr, "%s: Too less arguments.\n%s\n", pgm_name, usage);
+	if (parseInput(argc,argv,&tabstop,&firstFile) != 0){
+        (void) fprintf(stderr, "Fatal error. There was an error while parsing the user inputs. Cannot continue.");
         exit(EXIT_FAILURE);
-    }
-    //Check if the -t flag is set
-    if (argv[1][0] == '-'){
-		//We found a flag! Check if it is t
-		if(argv[1][1] == 't'){
-			long buff = strtol(argv[2],NULL,10);
-			if (buff >= 0 && buff <= INT_MAX) {
-				tabstop = buff;
-				firstFile = 3;
-			}
-			else {
-				(void) fprintf(stderr, "%s: The tabstop was too big.\n", pgm_name);
-				exit(EXIT_FAILURE);
-			}
-		}
-		//This was not the t flag
-		else{
-			(void) fprintf(stderr, "%s: The flag %c is unknown!\n%s\n", pgm_name,argv[1][1],usage);
-			exit(EXIT_FAILURE);
-		}
 	}
 
-	//Now check if all files exist
+	//open each file and replace all tabs with tabstop spaces
 	
-	for(int i = 0; i < argc - firstFile;++i)
-	{
-		if( access( argv[firstFile+i], W_OK  ) != -1 ) {
-			// file exists
-		} else {
-		    // file doesn't exist
-			(void) fprintf(stderr, "%s: The file %s does not exist or you don't have write permissions!\n%s\n", pgm_name,argv[firstFile+i],usage);
+	for(int i = 0; i < argc - firstFile;++i){
+		if(replaceTabsOfFile(argv[firstFile+i],tabstop) != 0){
+			(void) fprintf(stderr, "Fatal error. There was an error while writing to the file '%s'. Cannot continue.",argv[firstFile+i]);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -82,6 +57,17 @@ int main(int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
+/**
+ * Parse the user input of the program.
+ *
+ * @brief Parse the user input of the program
+ * @detail if the flag "-t" is set the "tabstop" variable get adjustet with the following value and the "firstFile" variable also is increased by 2. If there is a parsing error the program terminates.
+ * @param argc The number of command-line parameters in argv
+ * @param argv The array of command-line parameters, argc elements long.
+ * @param tabstop Address to the number of spaces which one tab should get replaced
+ * @param firstFile Address to the position of the first filename in the argv array 
+ * @return 0 on success, non-zero on failure.
+ */
 int parseInput(int argc, char **argv, unsigned int *tabstop, unsigned int *firstFile){
 	
     //Check if there are too less arguments
@@ -93,19 +79,25 @@ int parseInput(int argc, char **argv, unsigned int *tabstop, unsigned int *first
     if (argv[1][0] == '-'){
 		//We found a flag! Check if it is t
 		if(argv[1][1] == 't'){
+			//Check if there are filenames after the flag
+			if(argc < 4){
+				(void) fprintf(stderr, "%s: Too less arguments.\n%s\n", pgm_name, usage);
+				exit(EXIT_FAILURE);
+			}
+
 			long buff = strtol(argv[2],NULL,10);
 			if (buff >= 0 && buff <= INT_MAX) {
 				*tabstop = buff;
 				*firstFile = 3;
 			}
-			else {
-				(void) fprintf(stderr, "%s: The tabstop was too big.\n", pgm_name);
-				exit(EXIT_FAILURE);
-			}
 		}
 		//This was not the t flag
 		else{
 			(void) fprintf(stderr, "%s: The flag %c is unknown!\n%s\n", pgm_name,argv[1][1],usage);
+			exit(EXIT_FAILURE);
+		}
+		if(argc < 3){
+			(void) fprintf(stderr, "%s: Too less arguments.\n%s\n", pgm_name, usage);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -125,3 +117,29 @@ int parseInput(int argc, char **argv, unsigned int *tabstop, unsigned int *first
 	return 0;
 }
 
+
+int replaceTabsOfFile(const char* filename,const int tabstop){
+	int nChar;
+	FILE *file_open;
+	char tabs[tabstop];
+	//fill array tabs with spaces
+	for(int i = 0; i < tabstop; ++i){
+		tabs[i] = ' ';
+	}
+
+	if((file_open = fopen(filename, "r+")) == 0){
+		(void) fprintf(stderr, "%s: The file %s does not exist or you don't have write permissions!\n", pgm_name,filename);
+		return -1;
+	} 
+	// else it worked:
+	printf("File: %s opened\n",filename);
+	
+	while((nChar = fgetc(file_open)) != EOF){
+		if(nChar == '\t'){
+			fwrite(tabs, sizeof(tabs[0]),NRELEMENTS(tabs),file_open);	
+		}
+	}
+
+	fclose(file_open);
+	return 0;
+}
